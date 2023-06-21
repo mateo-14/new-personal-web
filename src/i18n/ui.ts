b5406530aa3effc0de768508a172e6014b54dd49
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2023-06-21 18:29:29 -0300

    i18n Update

---
+++ src/i18n/ui.ts
@@ -0,0 +1,32 @@
+export const languages = {
+  en: 'English',
+  es: 'Español',
+};
+
+export const defaultLang = 'en';
+
+export const ui = {
+  en: {
+    'nav.about': 'About me',
+    'nav.skills': 'Skills',
+    'nav.projects': 'Projects',
+    'nav.resume': 'Download resume',
+    'nav.contact': 'Contact',
+    'about.0': 'Hi 👋🏻, my name is Mateo Ledesma. I\'m 22 years old and I work full-time as a Frontend Developer, using JavaScript, React, and Next.js. Additionally, I\'m interested in expanding my skills and working as a Backend Developer in the future, using languages like C# or Go.',
+    'about.1': 'You can download my resume to learn more about me by',
+    'about.clickHere': 'clicking here',
+    'project.open': 'Open', 
+    
+  },
+  es: {
+    'nav.about': 'Sobre mí',
+    'nav.skills': 'Habilidades',
+    'nav.projects': 'Proyectos',
+    'nav.resume': 'Descargar CV',
+    'nav.contact': 'Contacto',
+    'about.0': 'Hola 👋🏻, mi nombre es Mateo Ledesma. Tengo 22 años y actualmente trabajo a tiempo completo (Full time) como Frontend Developer, utilizando JavaScript, React y Next.js. Además, tengo el interés de expandir mis habilidades y trabajar como Backend Developer en el futuro, utilizando lenguajes como C# o Go.',
+    'about.1': 'Podés descargar mi CV para conocer más sobre mí haciendo',
+    'about.clickHere': 'click acá',
+    'project.open': 'Abrir',
+  },
+} as const;
