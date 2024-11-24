32d3e42cc80776e5915584ec21b826efa0c6d4f3
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2024-11-24 01:35:06 -0300

    Update Background and set auto i18n routing

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,19 +1,11 @@
 import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
-import vercel from "@astrojs/vercel/serverless";
-
-export const defaultLocale = 'es'
-export const locales = ['es', 'en']
-
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  output: 'hybrid',
-  adapter: vercel(),
   i18n: {
-    defaultLocale,
-    locales,
-    routing: 'manual'
+    defaultLocale: 'es',
+    locales: ['es', 'en'],
   }
 });
