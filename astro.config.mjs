fa17bb205cd708047e594226b040c163b3364ce7
Mateo Ledesma <mateo.14.ledesma@gmail.com>
2024-11-24 01:10:46 -0300

    Update Astro and some improvements

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,9 +3,17 @@ import tailwind from "@astrojs/tailwind";
 
 import vercel from "@astrojs/vercel/serverless";
 
+export const defaultLocale = 'es'
+export const locales = ['es', 'en']
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
   output: 'hybrid',
-  adapter: vercel()
+  adapter: vercel(),
+  i18n: {
+    defaultLocale,
+    locales,
+    routing: 'manual'
+  }
 });
